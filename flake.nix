7613e1b9035adcc8318e739545a7cac168b5f9b3
Brian Tafoya <btafoya@briantafoya.com>
2026-01-19 09:39:32 -0500

    Fix URL for VSCode insider build in flake.nix
    Updated the URL for fetching the VSCode insider build.

--- flake.nix
+++ flake.nix
@@ -25,7 +25,7 @@
             (oldAttrs: {
               src = (builtins.fetchTarball {
                 url =
-                  "https://code.visualstudio.com/sha/download?build=insider&os=linux-x64";
+                  "https://code.visualstudio.com/sha/download?&os=linux-x64";
                 sha256 = "1fdjpm6i66n74cd2bfb2brmilj1842dig2fjxg4r9ximlkl0kf50";
               });
               version = "latest";
